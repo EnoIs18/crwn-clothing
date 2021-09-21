b3ad9548d5e45a6101a3391f83eb7e9d41fd2169
Eno Isallari <enoisallari18@gmail.com>
2021-09-21 16:14:35 +0200

    updating stripe and favicon

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,12 +6,9 @@ import rootReducer from './root-reducer';
 
 const middlewares = [logger];
 
-if (process.env.NODE_ENV === 'development') {
-  middlewares.push(logger);
-}
-
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 export const persistor = persistStore(store);
 
-export default { store, persistStore };
+export default  store 
+export  {persistStore} 
